c9c329cfc8235d99d75db8e407928ad00931889a
Christoffer Rydberg <christoffer@rekyl.se>
2014-09-03 15:22:47 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Random Ballzor!</title>
+
+    <link rel="stylesheet" href="css/app.css">
+    <script src="scripts/main.js"></script>
+</head>
+<body>
+    <div class="overlay">
+        <div class="gameover">
+            GAMEOVER!
+
+            <span class="points">0</span>
+        </div>
+    </div>
+    <div class="points">
+        <span class="points">0</span>p
+    </div>
+    <div class="timer">
+        <span class="timer">60</span>s kvar
+    </div>
+</body>
+</html>
